9aae59aa2b82ee7b602432ba62a18dd1509fab1d
pllensa <pllensa@myges.fr>
2025-04-18 15:04:22 +0200

    feat/remote: basic implementation

--- archivage.sh
+++ archivage.sh
@@ -2,12 +2,14 @@
 
 source archivage.var
 
+# overwriting of the variables for now
 LOCAL_BACKUP_DIR=/tmp/backups
 LOCAL_BACKUP_RET=/tmp/retention
 
 for i in $(find $LOCAL_BACKUP_DIR -mtime +$RETENTION_DAYS -exec echo {} \;); 
   do
   mkdir -p $LOCAL_BACKUP_RET/$(date --iso)
+
   cp $i $LOCAL_BACKUP_RET/$(date --iso)/$i
   if [ -z $? ]; then
     echo "$i wasn't copied to $LOCAL_BACKUP_RET/$(date --iso), check it."
@@ -15,4 +17,9 @@ for i in $(find $LOCAL_BACKUP_DIR -mtime +$RETENTION_DAYS -exec echo {} \;);
   if [ $(sha1 $i) -eq $(sha1 $LOCAL_BACKUP_RET/$(date --iso)/$i) ]; then
     rm $i
   fi
+
+  for i in "{REMOTE_REPO_SET[@]}"; do
+    ssh backup@i -e "mkdir $REMOTE_REPO_DIR/$(date --iso)"
+    rsync $i backup@$i:$REMOTE_REPO_DIR/$(date --iso)/$i
+  done
 done
