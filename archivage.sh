3ad467db008eec39a998f60abb5838881aec1d43
pllensa <pllensa@myges.fr>
2025-04-18 14:54:45 +0200

    dev 1

--- archivage.sh
+++ archivage.sh
@@ -2,6 +2,17 @@
 
 source archivage.var
 
-echo "${REMOTE_REPO_SET[0]}"
-echo "${REMOTE_REPO_SET[1]}"
-echo "${REMOTE_REPO_SET[2]}"
+LOCAL_BACKUP_DIR=/tmp/backups
+LOCAL_BACKUP_RET=/tmp/retention
+
+for i in $(find $LOCAL_BACKUP_DIR -mtime +$RETENTION_DAYS -exec echo {} \;); 
+  do
+  mkdir -p $LOCAL_BACKUP_RET/$(date --iso)
+  cp $i $LOCAL_BACKUP_RET/$(date --iso)/$i
+  if [ -z $? ]; then
+    echo "$i wasn't copied to $LOCAL_BACKUP_RET/$(date --iso), check it."
+  fi
+  if [ $(sha1 $i) -eq $(sha1 $LOCAL_BACKUP_RET/$(date --iso)/$i) ]; then
+    rm $i
+  fi
+done
